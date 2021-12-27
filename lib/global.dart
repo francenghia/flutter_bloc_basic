1323183e1166707b88cb87a9c6ccb41f6bd94631
xing.wang <wangxing.1211>
2021-12-27 17:13:29 +0800

    移除状态栏配置

--- lib/global.dart
+++ lib/global.dart
@@ -1,24 +1,14 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc_basic/utils/local_storage_util.dart';
 
 class Global {
   /// 初始化全局信息
   static Future init(VoidCallback callback,
-      {SystemUiOverlayStyle? androidStatusBarStyle,
-      VoidCallback? needInitialize}) async {
+      {VoidCallback? needInitialize}) async {
     WidgetsFlutterBinding.ensureInitialized();
     await LocalStorageUtil.instance();
     needInitialize?.call();
     callback();
-    if (Platform.isAndroid) {
-      androidStatusBarStyle ??= const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.dark,
-          statusBarBrightness: Brightness.dark);
-      SystemChrome.setSystemUIOverlayStyle(androidStatusBarStyle);
-    }
   }
 }
